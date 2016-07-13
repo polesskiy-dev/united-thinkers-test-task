f849011d8f4b87d462b34ccafd36ca0e903b8ed4
polesskiy-dev <e34dream@gmail.com>
2016-07-14 00:55:50 +0300

    Add readme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        org.junit.runner.JUnitCore.runClasses();
     }
 }
